1df7108bf9b67c8e5a667fb008726a55366ec832
Arthur Caumartin <artcaumart@gmail.com>
2026-04-02 18:29:15 +0200

    controler update

--- PlanetDash/Assets/[Project]/Scripts/Gameplay/CircularSurfaceMovement.cs
+++ PlanetDash/Assets/[Project]/Scripts/Gameplay/CircularSurfaceMovement.cs
@@ -1,12 +1,13 @@
+using System;
 using System.Collections;
 using UnityEngine;
 
 public abstract class CircularSurfaceMovement : MonoBehaviour
 {
-    [SerializeField] private float _movmentSpeed = 1;
+    [SerializeField] private float _movementSpeed = 1;
     [SerializeField] private float _jumpDuration = 0.5f;
     [SerializeField] private float _gravityScale = 1;
-    private float _currentAngle;
+    private float _currentAngleRadian;
     private float _jumpDistance;
     private float _altitude;
     private float _dynamiqueGravityScale;
@@ -24,24 +25,24 @@ public abstract class CircularSurfaceMovement : MonoBehaviour
     protected virtual void Update()
     {
         if (!planetSurface) return;
-        _currentAngle -= (velocity.x * _movmentSpeed * Time.deltaTime) / planetSurface.Radius
-                         * 180 / Mathf.PI; //* garentie le deplacement en m/s plutot qu'en deg/s
+        _currentAngleRadian -= (velocity.x * _movementSpeed * Time.deltaTime) / planetSurface.Radius;
+        //* garentie le deplacement en m/s plutot qu'en deg/s
         ComputePosition();
     }
 
     private void ComputeInitialeAngle()
     {
         Vector3 dirSurfaceToThis = (transform.position - planetSurface.transform.position).normalized;
-        _currentAngle = Vector3.SignedAngle(planetSurface.transform.right,
-                                            dirSurfaceToThis,
-                                            planetSurface.transform.forward);
+        _currentAngleRadian = -Vector3.SignedAngle(dirSurfaceToThis,
+                                                   planetSurface.transform.right,
+                                                   planetSurface.transform.forward) * Mathf.Deg2Rad;
     }
 
     private void ComputePosition()
     {
         Vector3 newPosition = new Vector3(
-            Mathf.Cos(_currentAngle * Mathf.Deg2Rad),
-            Mathf.Sin(_currentAngle * Mathf.Deg2Rad),
+            Mathf.Cos(_currentAngleRadian),
+            Mathf.Sin(_currentAngleRadian),
             0
         );
 
@@ -57,9 +58,8 @@ public abstract class CircularSurfaceMovement : MonoBehaviour
         }
 
         newPosition *= _altitude;
-
-        Vector3 worldPos = planetSurface.transform.InverseTransformPoint(newPosition);
-        transform.position = worldPos;
+        newPosition += planetSurface.transform.position;
+        transform.position = newPosition;
         transform.up = (transform.position - planetSurface.transform.position).normalized;
     }
 
@@ -71,7 +71,28 @@ public abstract class CircularSurfaceMovement : MonoBehaviour
 
     protected void Jump()
     {
-        if(!isGrounded) return;
+        if (!isGrounded) return;
         _altitude += 4;
+    }
+
+    public Vector3[] GetPathOnDirection(float direction, float distance, int resolution)
+    {
+        // return new Vector3[] { };
+        if (!planetSurface) return new Vector3[] { };
+
+        Vector3[] path = new Vector3[resolution];
+        float startRad = _currentAngleRadian;
+        float endRad = _currentAngleRadian + ((distance / planetSurface.Radius) * -direction);
+        for (int i = 0; i < resolution; i++)
+        {
+            float radTime = Mathf.Lerp(startRad, endRad, Mathf.InverseLerp(0, resolution, i));
+            Vector3 point = new Vector3(
+                Mathf.Cos(radTime),
+                Mathf.Sin(radTime),
+                0
+            );
+            path[i] = (point * (_altitude + 2)) + planetSurface.transform.position;
+        }
+        return path;
     }
 }
--- PlanetDash/Assets/[Project]/Scripts/Gameplay/PlayerMovement.cs
+++ PlanetDash/Assets/[Project]/Scripts/Gameplay/PlayerMovement.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -19,9 +18,39 @@ public class PlayerMovement : CircularSurfaceMovement
         _inputDirection = inputDir;
     }
 
+    private void Dash(InputValue value)
+    {
+        if(value.Get<float>() > .5f)
+        {
+            
+        }
+    }
+
     private void OnJump(InputValue value)
     {
         if (value.Get<float>() > .5f)
             Jump();
     }
+
+    private void OnDrawGizmos()
+    {
+        Vector3[] path = GetPathOnDirection(_inputDirection.x, 20, 10);
+        if(path.Length == 0) return;
+        print("count : " + path.Length);
+        foreach (var item in path)
+            print("path : " + item);
+        Gizmos.color = Color.blue;
+        Gizmos.DrawSphere(path[0], 1);
+        Gizmos.color = Color.green;
+        Gizmos.DrawSphere(path[path.Length - 1], 1);
+        for (int i = 0; i < path.Length - 1; i++)
+        {
+            Gizmos.color = i % 2 == 0 ? Color.green : Color.yellow;
+            Gizmos.DrawLine(path[i], path[i + 1]);
+        }
+    }
 }
+
+
+
+
