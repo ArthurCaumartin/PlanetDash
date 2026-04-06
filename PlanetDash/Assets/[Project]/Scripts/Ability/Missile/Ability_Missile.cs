9f7e49cd1bd3e14995685c3bb4a330e341b71787
Arthur Caumartin <artcaumart@gmail.com>
2026-04-06 06:00:56 +0200

    add missile Instantiate from Missile ability

--- PlanetDash/Assets/[Project]/Scripts/Ability/Missile/Ability_Missile.cs
+++ PlanetDash/Assets/[Project]/Scripts/Ability/Missile/Ability_Missile.cs
@@ -1,15 +1,21 @@
 
+using UnityEditor;
 using UnityEngine;
 
 public class Ability_Missile : AbilityDecorator
 {
     public Ability_Missile(IAbility ability) : base(ability) { }
 
-    public override void OnDashHit(Health health)
+    public override void OnDashHit(Health healthHit, Health[] healthsHitArray)
     {
-        base.OnDashHit(health);
-        GameObject go = new GameObject("Missile");
-        go.transform.position = health.transform.position;
+        base.OnDashHit(healthHit, healthsHitArray);
+        //TODO horrible de get la ref avec un LoadAsset o_O A CHANGER !!!! (je sais pas comment...)
+        GameObject mRef = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/[Project]/Prefabs/Projectiles/Missile.prefab");
+        for (int i = 0; i < 10; i++)
+        {
+            GameObject m = GameObject.Instantiate(mRef, healthHit.transform.position + healthHit.transform.up, healthHit.transform.rotation);
+            m.GetComponent<Missile>().Init(healthsHitArray.GetRandome(), 50, 10, 150, 3);
+        }
     }
 }
 
