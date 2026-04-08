68457e696a9b439098384be220da489792314fca
Arthur Caumartin <artcaumart@gmail.com>
2026-04-08 23:48:45 +0200

    chore: oups... franglai issue

--- PlanetDash/Assets/[Project]/Scripts/Utils/ArrayUtils.cs
+++ PlanetDash/Assets/[Project]/Scripts/Utils/ArrayUtils.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public static class ArrayUtils
 {
-    public static T GetRandome<T>(this T[] array)
+    public static T GetRandom<T>(this T[] array)
     {
         return array[Random.Range(0, array.Length)];
     }
