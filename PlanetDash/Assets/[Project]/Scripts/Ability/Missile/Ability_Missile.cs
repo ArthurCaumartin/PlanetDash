38557c1d63574624687f885b68d3fcdaa5dbe5a8
Arthur Caumartin <artcaumart@gmail.com>
2026-04-16 01:05:45 +0200

    chior: add inheritance support to Missile

--- PlanetDash/Assets/[Project]/Scripts/Ability/Missile/Ability_Missile.cs
+++ PlanetDash/Assets/[Project]/Scripts/Ability/Missile/Ability_Missile.cs
@@ -1,18 +1,17 @@
-
 using System;
 using UnityEngine;
 
 [Serializable]
 public class Ability_Missile : AbilityDecorator
 {
-    private Missile _missilePrefab;
-    public float damage;
-    public int missileCount;
+    [SerializeField] protected Missile missilePrefab;
+    [SerializeField] public float damage;
+    [SerializeField] public int missileCount = 1;
 
     public Ability_Missile(IAbility ability, Missile missilePrefab) : base(ability)
     {
-        Debug.Log("Construct Ability Missile !");
-        _missilePrefab = missilePrefab;
+        // Debug.Log("Construct Ability Missile !");
+        this.missilePrefab = missilePrefab;
     }
 
     public void SetStat(float damage, int missileCount)
@@ -23,17 +22,25 @@ public class Ability_Missile : AbilityDecorator
 
     public override void OnDashHit(Health healthHit, Health[] healthsHitArray)
     {
+        // Debug.Log("Missile : OnDash " + missileCount);
         base.OnDashHit(healthHit, healthsHitArray);
 
         for (int i = 0; i < missileCount; i++)
         {
-            Missile m = GameObject.Instantiate(_missilePrefab,
-                                            healthHit.transform.position + healthHit.transform.up,
-                                            healthHit.transform.rotation
-            );
-
-            m.Init(healthsHitArray.GetRandome(), 50, 10, 150, 3);
+            SpawnMissile(healthHit.transform, healthsHitArray.GetRandom());
         }
     }
-}
 
+    protected void SpawnMissile(Vector3 position, Quaternion rotation, Health target)
+    {
+        // Debug.Log("Spawn Missile : " + missilePrefab.name + " : target : " + target.name);
+        Missile missile = GameObject.Instantiate(missilePrefab, position, rotation);
+        missile.Init(target, 50, 10, 150, 3);
+    }
+
+    protected void SpawnMissile(Transform spawnTransform, Health target)
+    {
+        Missile missile = GameObject.Instantiate(missilePrefab, spawnTransform.position + spawnTransform.up, spawnTransform.rotation);
+        missile.Init(target, 50, 10, 150, 3);
+    }
+}
