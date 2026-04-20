e7798e7a09bf15e2e939caada1a81175f417d902
Arthur Caumartin <artcaumart@gmail.com>
2026-04-20 23:39:51 +0200

    feat: add random walkink direction for mobs un start

--- PlanetDash/Assets/[Project]/Scripts/GameLoop/MobMovement.cs
+++ PlanetDash/Assets/[Project]/Scripts/GameLoop/MobMovement.cs
@@ -1,13 +1,12 @@
+using UnityEngine;
+
 public class MobMovement : CircularSurfaceMovement
 {
+    private int _direction;
     protected override void Start()
     {
         base.Start();
-        // ComputePositionAndRotation(currentAngleRadian);
-    }
-
-    protected override void Update()
-    {
-        base.Update();
+        _direction = Random.value > .5f ? 1 : -1;
+        velocity.x = _direction;
     }
 }
