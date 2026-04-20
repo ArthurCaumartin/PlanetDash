15f418e37130e7e80aaa103d48de8c634436f883
Arthur Caumartin <artcaumart@gmail.com>
2026-04-20 23:37:05 +0200

    chore: use ScriptableAbility in ScriptableUpgrade and in PlayerUpgradeControler

--- PlanetDash/Assets/[Project]/Scripts/Upgrades/PlayerUpgradeControler.cs
+++ PlanetDash/Assets/[Project]/Scripts/Upgrades/PlayerUpgradeControler.cs
@@ -3,12 +3,14 @@ using UnityEngine;
 
 public class PlayerUpgradeControler : MonoBehaviour
 {
+    [SerializeField] private ScriptablePlayerData _playerData;
     [SerializeField] private AbilityControler _abilityControler;
     [SerializeField] private List<ScriptableUpgrade> _startUpgrade = new List<ScriptableUpgrade>();
     [SerializeField] private List<ScriptableUpgrade> _upgradeList = new List<ScriptableUpgrade>();
 
     private void Start()
     {
+        _playerData.ClearData();
         foreach (var item in _startUpgrade)
             SetNewUpgrade(item);
     }
@@ -16,6 +18,7 @@ public class PlayerUpgradeControler : MonoBehaviour
     public void SetNewUpgrade(ScriptableUpgrade newUpgrade)
     {
         _upgradeList.Add(newUpgrade);
-        _abilityControler.AddAbility(newUpgrade.Ability);
+        _playerData.RecordNewUpgrade(newUpgrade);
+        _abilityControler.AddAbility(newUpgrade.AbilityData.Ability);
     }
 }
--- PlanetDash/Assets/[Project]/Scripts/Upgrades/ScriptableUpgrade.cs
+++ PlanetDash/Assets/[Project]/Scripts/Upgrades/ScriptableUpgrade.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Upgrade_", menuName = "Mwa/Upgrade")]
 public class ScriptableUpgrade : ScriptableObject
 {
-    [SerializeField] private Ability _abilityPrefab;
-
+    [SerializeField] private ScriptableAbility _scriptableAbility;
     [SerializeField] private int _strenght = 0;
     [SerializeField] private int _intelligence = 0;
     [SerializeField] private int _dexterity = 0;
-    [SerializeField] private int _luck = 0;
+
+    public int Strenght => _strenght;
+    public int Intelligence => _intelligence;
+    public int Dexterity => _dexterity;
 
 
-    public Ability Ability => _abilityPrefab;
+    public ScriptableAbility AbilityData => _scriptableAbility;
 }
